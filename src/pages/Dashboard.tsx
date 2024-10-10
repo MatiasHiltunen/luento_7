240996c126eee28eb1975b79e66b2d1dbd470292
matiashiltunen <26164588+MatiasHiltunen@users.noreply.github.com>
2024-10-10 11:08:54 +0300

    remove code

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -107,10 +107,7 @@ export function Dashboard() {
                   ))}
                 </TableBody>
                 <TableFooter>
-                  <TableRow>
-                    <TableCell colSpan={3}>Total</TableCell>
-                    <TableCell className="text-right">$2,500.00</TableCell>
-                  </TableRow>
+                 
                 </TableFooter>
               </Table>
             </CardContent>
